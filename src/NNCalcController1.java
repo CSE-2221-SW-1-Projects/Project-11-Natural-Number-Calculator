422889262ee7bb28884db09f3c256479b26ed406
Zheyuan Gao <90584603+JellyFishGao@users.noreply.github.com>
2023-07-30 21:28:18 +0800

    Add files via upload

---
+++ src/NNCalcController1.java
@@ -0,0 +1,286 @@
+import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
+
+/**
+ * Controller class.
+ *
+ * @author Put your name here
+ */
+public final class NNCalcController1 implements NNCalcController {
+
+    /**
+     * Model object.
+     */
+    private final NNCalcModel model;
+
+    /**
+     * View object.
+     */
+    private final NNCalcView view;
+
+    /**
+     * Useful constants.
+     */
+    private static final NaturalNumber TWO = new NaturalNumber2(2),
+            INT_LIMIT = new NaturalNumber2(Integer.MAX_VALUE);
+
+    /**
+     * Updates this.view to display this.model, and to allow only operations
+     * that are legal given this.model.
+     *
+     * @param model
+     *            the model
+     * @param view
+     *            the view
+     * @ensures [view has been updated to be consistent with model]
+     */
+    private static void updateViewToMatchModel(NNCalcModel model,
+            NNCalcView view) {
+
+        //get the input natural number from input
+        NaturalNumber input = model.top();
+        NaturalNumber output = model.bottom();
+        //displays the input and output to the view
+        view.updateTopDisplay(input);
+        view.updateBottomDisplay(output);
+
+        //check the condition for subtract. requires top >= bottom
+        if (model.top().compareTo(model.bottom()) >= 0) {
+            view.updateSubtractAllowed(true);
+        } else {
+            view.updateSubtractAllowed(false);
+        }
+
+        //check the condition for division, requires bottom > 0
+        if (model.bottom().isZero()) {
+            view.updateDivideAllowed(false);
+        } else {
+            view.updateDivideAllowed(true);
+        }
+
+        //check the condition for power, requires bottom <= INT_LIMIT
+        if (model.bottom().compareTo(INT_LIMIT) > 0) {
+            view.updatePowerAllowed(false);
+        } else {
+            view.updatePowerAllowed(true);
+        }
+
+        //check the condition for root, requires 2 <= bottom <= INT_LIMIT
+        if (model.bottom().compareTo(TWO) >= 0
+                && model.bottom().compareTo(INT_LIMIT) <= 0) {
+            view.updateRootAllowed(true);
+        } else {
+            view.updateRootAllowed(false);
+        }
+
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param model
+     *            model to connect to
+     * @param view
+     *            view to connect to
+     */
+    public NNCalcController1(NNCalcModel model, NNCalcView view) {
+        this.model = model;
+        this.view = view;
+        updateViewToMatchModel(model, view);
+    }
+
+    @Override
+    public void processClearEvent() {
+        /*
+         * Get alias to bottom from model
+         */
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        bottom.clear();
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+    }
+
+    @Override
+    public void processSwapEvent() {
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        NaturalNumber temp = top.newInstance();
+        temp.transferFrom(top);
+        top.transferFrom(bottom);
+        bottom.transferFrom(temp);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+    }
+
+    @Override
+    public void processEnterEvent() {
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        top.copyFrom(bottom);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+    }
+
+    @Override
+    public void processAddEvent() {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        bottom.add(top);
+        top.clear();
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processSubtractEvent() {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        top.subtract(bottom);
+        bottom.transferFrom(top);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processMultiplyEvent() {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        bottom.multiply(top);
+        top.clear();
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processDivideEvent() {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        NaturalNumber remainder = top.divide(bottom);
+        bottom.transferFrom(top);
+        top.transferFrom(remainder);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processPowerEvent() {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        int power = bottom.toInt();
+        top.power(power);
+        bottom.transferFrom(top);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processRootEvent() {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        int root = bottom.toInt();
+        top.root(root);
+        bottom.transferFrom(top);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processAddNewDigitEvent(int digit) {
+
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        bottom.multiplyBy10(digit);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+}
